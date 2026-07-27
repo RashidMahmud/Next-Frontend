28bfb598f27072ddccd5215f85d91c6a9b21b554
RashidMahmud <rashidmahmud999@gmail.com>
2026-07-28 01:31:34 +0600

    Refactor code structure for improved readability and maintainability

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import { Raleway } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const raleway = Raleway({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={` h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", raleway.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
